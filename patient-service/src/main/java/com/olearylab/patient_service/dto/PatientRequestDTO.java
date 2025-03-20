b15dc0b58903e58f5531045b06eb4d1ba1cb53b8
cian-oL <cianoleary2010@gmail.com>
2025-03-20 08:35:19 +0000

    Added create patient functionality with exception handling of request validation

---
+++ patient-service/src/main/java/com/olearylab/patient_service/dto/PatientRequestDTO.java
@@ -0,0 +1,28 @@
+package com.olearylab.patient_service.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PatientRequestDTO {
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name must be at most 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be a valid email address")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "Date of birth is required")
+    private String dateOfBirth;
+
+    @NotBlank(message = "Date of registration is required")
+    private String dateOfRegistration;
+}
--- patient-service/src/main/java/com/olearylab/patient_service/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/olearylab/patient_service/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.olearylab.patient_service.mapper;
 
+import com.olearylab.patient_service.dto.PatientRequestDTO;
 import com.olearylab.patient_service.dto.PatientResponseDTO;
 import com.olearylab.patient_service.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
@@ -13,5 +16,16 @@ public class PatientMapper {
         dto.setDateOfBirth(patient.getDateOfBirth().toString());
 
         return dto;
+    }
+
+    public static Patient toModel(PatientRequestDTO dto) {
+        Patient patient = new Patient();
+        patient.setName(dto.getName());
+        patient.setEmail(dto.getEmail());
+        patient.setAddress(dto.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(dto.getDateOfBirth()));
+        patient.setDateOfRegistration(LocalDate.parse(dto.getDateOfRegistration()));
+
+        return patient;
     }
 }
