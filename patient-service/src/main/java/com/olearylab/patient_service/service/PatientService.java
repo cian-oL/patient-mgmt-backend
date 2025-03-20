51e899e5096824a5cd4ce70288de66d92b00f5ac
cian-oL <cianoleary2010@gmail.com>
2025-03-20 14:11:12 +0000

    Added check for existing for createPatient with exception handling

--- patient-service/src/main/java/com/olearylab/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/olearylab/patient_service/service/PatientService.java
@@ -2,6 +2,7 @@ package com.olearylab.patient_service.service;
 
 import com.olearylab.patient_service.dto.PatientRequestDTO;
 import com.olearylab.patient_service.dto.PatientResponseDTO;
+import com.olearylab.patient_service.exception.EmailAlreadyExistsException;
 import com.olearylab.patient_service.mapper.PatientMapper;
 import com.olearylab.patient_service.model.Patient;
 import com.olearylab.patient_service.repository.PatientRepository;
@@ -20,6 +21,10 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        if (patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+            throw new EmailAlreadyExistsException(patientRequestDTO.getEmail());
+        }
+
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
 
         return PatientMapper.toDTO(newPatient);
